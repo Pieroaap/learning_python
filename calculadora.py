e3c2ad36dd40d8b6a1c1540ad6b2fad1ec944f3d
Piero Alvarado Palomino <pieroalexis33@gmail.com>
2025-01-16 01:05:28 -0500

    Se modifica calculadora por codigo de Hola Mundo

--- calculadora.py
+++ calculadora.py
@@ -1,23 +1,32 @@
 def calculadora():
     bienvenida = "Bienvenido a la calculadora \nPara salir escribre Salir \nLas operaciones que puedes realizas son suma, multi, div y resta"
     print(bienvenida)
-    n1 = input("Ingrese un número: ")
-    if(n1 == 'Salir'):
-        print("Adios")
-        return
-    else:
+    resultado = ""
+    while True:
+        if not resultado:
+            resultado = input("Ingrese un número: ")
+            if resultado.lower() == 'salir':
+                print("Adios")
+                break
+            resultado = int(resultado)
+        op = input("Ingrese la operación que desea realizar: ")
+        if op.lower() == 'salir':
+            print("Adios")
+            break
         n2 = input("Ingrese otro número: ")
-        operacion = input("Ingrese la operación que desea realizar: ")
-        if(operacion == 'suma'):
-            print("El resultado de la suma es: ", float(n1) + float(n2))
-        elif(operacion == 'resta'):
-            print("El resultado de la resta es: ", float(n1) - float(n2))
-        elif(operacion == 'multi'):
-            print("El resultado de la multiplicación es: ", float(n1) * float(n2))
-        elif(operacion == 'div'):
-            print("El resultado de la división es: ", float(n1) / float(n2))
+        if(op.lower() == 'suma'):
+            resultado += int(n2)
+        elif(op.lower() == 'resta'):
+            resultado -= int(n2)
+        elif(op.lower() =='multi'):
+            resultado *= int(n2)
+        elif(op.lower() == 'div'):
+            resultado /= int(n2)
         else:
             print("Operación no válida")
+            break
+
+        print(f"El resultado es: {resultado}")
 
 calculadora()
 
